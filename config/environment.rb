9c266995835ebb75bd8cea64d2850e8f2770f1e9
Andrei Popovici <andrei@adyus.com>
2013-11-23 23:12:18 +0000

    =S3 uploads still not working

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+AWS::S3::DEFAULT_HOST = "winzo.s3.amazonaws.com"
+
 # Initialize the rails application
 Winzo::Application.initialize!
